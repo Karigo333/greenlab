119e9b7a2fd53f34d93894a911290886e742ac92
karigo <igorokkarpovich99@gmail.com>
2023-11-22 23:22:38 +0200

    add topbar

---
+++ src/app/components/topbar/topbar.conponent.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-topbar',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: './topbar.component.html',
+    styleUrl: './topbar.component.scss'
+})
+
+export class TopbarComponent {
+    title = 'topbar';
+}
