9340b535a91b9c675678c607676ba9519894f955
karigo <igorokkarpovich99@gmail.com>
2023-11-23 21:48:56 +0200

    add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TopbarComponent } from './components/topbar/topbar.conponent';
+import { HeaderComponent } from './components/header/header.conponent';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TopbarComponent],
+  imports: [CommonModule, RouterOutlet, TopbarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    fontFamily: {
+      raleway: ['Raleway'],
+    },
     container: {
-      center: true
+      center: true,
+      padding: '8rem',
+    },
+    padding: {
+      DEFAULT: '1rem',
+      sm: '2rem',
+      lg: '4rem',
+      xl: '5rem',
+      '2xl': '6rem',
     },
     extend: {},
   },
