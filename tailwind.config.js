f56dfa42803761dfe4549852f0a8fb3ae34b2cf2
karigo <igorokkarpovich99@gmail.com>
2023-12-03 22:34:35 +0200

    mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,27 +30,30 @@ module.exports = {
       nunito: ['Nunito'],
       montserrat: ['Montserrat'],
       montserrat_alternates: ['Montserrat Alternates'],
+      jost: ['Jost'],
     },
     container: {
+      screens: {
+        "ss": '300px',
+        // => @media (min-width: 300px) { ... }
+
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
       center: true,
-      padding: '8rem',
-    },
-    screens: {
-      "ss": '300px',
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
 
-      'md': '900px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
     },
     extend: {},
   },
