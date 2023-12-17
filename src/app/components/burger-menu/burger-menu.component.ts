cf75222164b3cef28e17dd4fa904052659bf383b
karigo <igorokkarpovich99@gmail.com>
2023-12-17 22:19:43 +0200

    burger

--- src/app/components/burger-menu/burger-menu.component.ts
+++ src/app/components/burger-menu/burger-menu.component.ts
@@ -5,6 +5,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 
 interface MenuItem {
   title: string;
+  link?: string;
   children?: MenuItem[];
   isOpen?: boolean; // Добавлено новое свойство
 }
@@ -37,18 +38,113 @@ export class BurgerMenuComponent {
   isOpen: boolean = false;
 
   menuItems: MenuItem[] = [
-    { title: 'Home' },
-    { title: 'About' },
+    { title: 'Головна',
+      link: '/'
+    },
     {
-      title: 'Services',
+      title: 'Правила збіру проб ❯',
       children: [
-        { title: 'Web Development' },
-        { title: 'Mobile App Development' },
-        { title: 'UI/UX Design' }
+        { title: 'Підготовка тварини до аналізу',
+          link: '/preparation' 
+        },
+        { title: 'Правила збіру проб',
+          link: '/rules'
+        },
+        { title: 'Маркування  проб',
+          link: '/marking'
+        },
+        { title: 'Витратні матеріали',
+          link: '/materials'
+        },
+        { 
+          title: 'Відбір ❯',
+          children: [
+            { title: 'Вибір крові',
+              link: '/blood-sampling'
+            },
+            { title: 'Змив з кон’юнктиви',
+              link: '/conjunctiva-sampling'
+            },
+            { title: 'Змив з носа',
+              link: '/nose-sampling'
+            },
+            { title: 'Зішкріб зі слизової урогенітального тракту, піхви',
+              link: '/vagina-sampling'
+            },
+            { title: 'Зішкріб зі слизової прямої кишки',
+              link: '/rectal-sampling'
+            },
+            { title: 'Змив з ротової порожнини, глотки',
+              link: '/oral-sampling'
+            },
+            { title: 'Відбір випоту (грудного, черевного)',
+              link: '/discharge-sampling'
+            }
+          ]
+        }
       ],
       isOpen: false
     },
-    { title: 'Contact' }
+    {
+      title: 'Послуги ❯',
+      children: [
+        { title: 'Що таке ПЛР?',
+          link: '/info'
+        },
+        { title: 'Прайс лист',
+          link: '/'
+        },
+        { title: 'Діагностика ПЛР ❯',
+          children: [
+            { title: 'Для котів',
+              link: '/diagnostics-plr-cats'
+            },
+            { title: 'Для собак',
+              link: '/diagnostics-plr-dogs'
+            },
+            { title: 'Для тхорів',
+              link: '/diagnostics-plr-ferrets'
+            }
+          ]
+        },
+        { title: 'Діагностичні панелі ПЛР ❯',
+          children: [
+          { title: 'Для котів',
+            link: '/diagnostics-panel-cats'
+          },
+          { title: 'Для собак',
+            link: '/diagnostics-panel-dogs'
+          }
+          ] 
+        },
+        { title: 'Гормони',
+          link: '/hormones'
+        },
+        { title: 'Генетичні дослідження',
+          link: '/genetics'
+        },
+        { title: 'Бакпосів',
+          link: '/batrerium-research'
+        },
+        { title: 'Маркери запалення (новинка) ❯',
+          children: [
+            { title: 'Для котів',
+              link: '/marker-cats'
+            },
+            { title: 'Для собак',
+              link: '/marker-dogs'
+            }
+          ]
+        }
+      ],
+      isOpen: false
+    },
+    { title: 'Як здати аналіз?',
+      link: '/guide'
+    },
+    { title: 'Контакти',
+      link: '/contact'
+    }
   ];
 
   constructor() { }
