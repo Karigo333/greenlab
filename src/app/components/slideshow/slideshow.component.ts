50f7eabdbdafd7147dad0fb4b3c0a837ae0389ac
Karigo333 <igorokkarpovich99@gmail.com>
2024-09-26 11:37:24 +0300

    fix slideshow

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,18 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit, AfterViewInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';  // Импортируйте нужные модули для standalone компонента
 
 @Component({
   selector: 'app-slideshow',
-  standalone: true,
-  imports: [CommonModule],
+  standalone: true,  // Это делает компонент автономным
+  imports: [CommonModule],  // Импортируйте общие модули, если необходимо (например, ngFor, ngIf)
   templateUrl: './slideshow.component.html',
-  styleUrl: './slideshow.component.scss'
+  styleUrls: ['./slideshow.component.scss']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
   @Input() images: string[] = [];
   slideIndex = 1;
 
-  ngOnInit() {
+  @ViewChildren('mySlides') slideElements!: QueryList<ElementRef>;
+  @ViewChildren('dot') dotElements!: QueryList<ElementRef>;
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
     this.showSlides(this.slideIndex);
   }
 
@@ -25,22 +30,21 @@ export class SlideshowComponent implements OnInit {
   }
 
   showSlides(n: number) {
-    let i;
-    const slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
-    const dots = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
+    const slides = this.slideElements.toArray();
+    const dots = this.dotElements.toArray();
 
     if (n > slides.length) { this.slideIndex = 1; }
     if (n < 1) { this.slideIndex = slides.length; }
 
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
+    slides.forEach((slide, index) => {
+      slide.nativeElement.style.display = 'none';
+    });
 
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
+    dots.forEach((dot, index) => {
+      dot.nativeElement.className = dot.nativeElement.className.replace(' active', '');
+    });
 
-    slides[this.slideIndex - 1].style.display = "block";
-    dots[this.slideIndex - 1].className += " active";
+    slides[this.slideIndex - 1].nativeElement.style.display = 'block';
+    dots[this.slideIndex - 1].nativeElement.className += ' active';
   }
 }
